a6a14cd81bf63f3137690a92220b2929f1e65f8b
dev2033 <dev.elop.02@mail.ru>
2021-01-28 21:50:45 +0300

    add js_practic

---
+++ js_practice/script.js
@@ -0,0 +1,62 @@
+// function fun1() {
+//     /*
+//      * Работа с checkbox 
+//      */
+//     var chbox;
+//     chbox = document.getElementById('one');
+
+//     if (chbox.checked) {
+//         alert('Выбран!');
+//     } else {
+//         alert('Не выбран!');
+//     }
+// }
+
+
+// function fun2() {
+//     /*
+//      * Работа с radio button 
+//      */
+//     var radio = document.getElementsByName('r1');
+//     for (var i = 0; i < radio.length; i++) {
+//         if (radio[i].checked) {
+//             alert('Выбран ' + i + ' элемент');
+//         }
+//     }
+// }
+
+
+
+// function fun3() {
+//     /*
+//      * Работа с выпадающим списком 
+//      */
+//     var sel = document.getElementById('mySelect').selectedIndex;
+//     var options = document.getElementById('mySelect').options;
+//     alert('Выбрана опция ' + options[sel].text);
+// }
+
+// работа с input range
+// function fun4() {
+//     // var rng = document.getElementById('r1');
+//     // var p = document.getElementById('one');
+//     // p.innerHTML = rng.value;
+
+
+//     // var i1 = document.getElementById('i1');
+//     // i1.value = rng.value;
+
+
+//     var rng = document.getElementById('r1');
+//     var div = document.getElementById('test');
+//     div.style.width = rng.value + 'px';
+// }
+
+
+
+function fun5() {
+    /*
+     * CSS генератор 
+     */
+
+}
