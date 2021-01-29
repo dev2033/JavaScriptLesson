e54af54c777369d2a53bddbb2aed2832dacd536a
dev2033 <dev.elop.02@mail.ru>
2021-01-29 18:17:12 +0300

    add tabws

--- js_practice/script.js
+++ js_practice/script.js
@@ -54,26 +54,111 @@
 
 
 
-function fun5() {
-    /*
-     * CSS генератор 
-     */
-    var rtl = document.getElementById('rtl').value;
-    var rtr = document.getElementById('rtr').value;
-    var rbr = document.getElementById('rbr').value;
-    var rbl = document.getElementById('rbl').value;
+// function fun5() {
+//     /*
+//      * CSS генератор 
+//      */
+//     var rtl = document.getElementById('rtl').value;
+//     var rtr = document.getElementById('rtr').value;
+//     var rbr = document.getElementById('rbr').value;
+//     var rbl = document.getElementById('rbl').value;
 
-    var ttl = document.getElementById('ttl');
-    var ttr = document.getElementById('ttr');
-    var tbr = document.getElementById('tbr');
-    var tbl = document.getElementById('tbl');
+//     var ttl = document.getElementById('ttl');
+//     var ttr = document.getElementById('ttr');
+//     var tbr = document.getElementById('tbr');
+//     var tbl = document.getElementById('tbl');
 
-    var block = document.getElementById('block');
+//     var block = document.getElementById('block');
 
-    ttl.value = rtl;
-    ttr.value = rtr;
-    tbr.value = rbr;
-    tbl.value = rbl;
+//     ttl.value = rtl;
+//     ttr.value = rtr;
+//     tbr.value = rbr;
+//     tbl.value = rbl;
 
-    block.style.borderRadius = rtl + 'px ' + rtr + 'px ' + rbr + 'px ' + rbl + 'px ';
+//     block.style.borderRadius = rtl + 'px ' + rtr + 'px ' + rbr + 'px ' + rbl + 'px ';
+// }
+
+
+/*
+ * Выпадающее меню ------------------------------------------ 
+ */
+
+/*
+document.getElementById('nav').onmouseover = function(event) {
+    var target = event.target;
+    if (target.className == 'menu-item') {
+        var s = target.getElementsByClassName('submenu');
+        closeMenu();
+        s[0].style.display = 'block';
+
+    }
 }
+
+document.onmouseover = function(event) {
+    var target = event.target;
+    console.log(event.target);
+    if (target.className != 'menu-item' && target.className != 'submenu') {
+        closeMenu();
+    }
+}
+
+function closeMenu() {
+    var menu = document.getElementById('nav');
+    var subm = document.getElementsByClassName('submenu');
+    for (var i = 0; i < subm.length; i++) {
+        subm[i].style.display = 'none';
+    }
+}
+*/
+
+/*
+ * Выпадающее меню ------------------------------------------ 
+ */
+
+
+
+/*
+ * Вкладки табы (Tabs) 
+ */
+
+var tab;
+var tabContent;
+
+window.onload = function() {
+    tabContent = document.getElementsByClassName('tabContent');
+    tab = document.getElementsByClassName('tab');
+    hideTabsContent(1);
+}
+
+function hideTabsContent(a) {
+    for (var i = 0; i < tabContent.length; i++) {
+        tabContent[i].classList.remove('show');
+        tabContent[i].classList.add('hide');
+        tab[i].classList.remove('whiteborder');
+    }
+}
+
+document.getElementById('tabs').onclick = function(event) {
+    var target = event.target;
+    if (target.className == 'tab') {
+        for (var i = 0; i < tab.length; i++) {
+            if (target == tab[i]) {
+                showTabsContent(i);
+                break;
+            }
+        }
+    }
+}
+
+function showTabsContent(b) {
+    if (tabContent[b].classList.contains('hide')) {
+        hideTabsContent(0);
+        tab[b].classList.add('whiteborder');
+        tabContent[b].classList.remove('hide');
+        tabContent[b].classList.add('show');
+    }
+}
+
+/*
+ * Вкладки табы (Tabs) 
+ */
